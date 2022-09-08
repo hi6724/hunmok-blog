3d29326c86a0a478e7375843431227fb0a90b5a3
hi6724 <hi6724@gmail.com>
2022-09-08 20:44:42 +0900

    dump data 생성

---
+++ src/test/blogData.ts
@@ -0,0 +1,12 @@
+export const blogData = [
+  { title: "욕심쟁이 판다", payload: "", type: "algorithm" },
+  { title: "firebase 사용법", payload: "", type: "backend" },
+  { title: "징검다리 건너기", payload: "", type: "algorithm" },
+  { title: "redux 에서 비동기처리", payload: "", type: "frontend" },
+  { title: "아기상어", payload: "", type: "algorithm" },
+  { title: "useRef 를 사용하는 이유", payload: "", type: "frontend" },
+  { title: "LCS", payload: "", type: "algorithm" },
+  { title: "typescript 타입지정", payload: "", type: "frontend" },
+  { title: "리액트 렌더링 최적화", payload: "", type: "frontend" },
+  //   { title: "useMemo vs React.memo", payload: "", type: "backend" },
+];
