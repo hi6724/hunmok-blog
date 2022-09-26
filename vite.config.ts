d46263b0b840df5abe40f434e54be455524b675b
hi6724 <hi6724@gmail.com>
2022-09-26 17:50:05 +0900

    build path

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    commonjsOptions: {
-      strictRequires: true,
-      ignoreGlobal: true,
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      output: {
-        globals: {
-          react: "react",
-        },
-      },
-    },
-  },
   define: {
     global: "window",
   },
