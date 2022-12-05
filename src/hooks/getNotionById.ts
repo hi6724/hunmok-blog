b7bb82b55cf5e0106901823518ba94a78fdeb6be
hi6724 <hi6724@gmail.com>
2022-12-05 15:14:31 +0900

    일단 render 로 설정

--- src/hooks/getNotionById.ts
+++ src/hooks/getNotionById.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const getNotionById = async (id: string) => {
-  const { data } = await axios.get(`http://huntree.cafe24app.com/notion/${id}`);
+  const { data } = await axios.get(
+    `https://notion-api.onrender.com/notion/${id}`
+  );
   return data;
 };
 
